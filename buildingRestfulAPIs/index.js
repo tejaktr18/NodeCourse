dc04c56279f1a2483131e996453abc62d12a343a
tejaktr18 <tejaktr18@gmail.com>
2022-06-25 00:52:27 -0400

    express framework object created

---
+++ buildingRestfulAPIs/index.js
@@ -0,0 +1,28 @@
+// const http = require('http');
+
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/'){
+//         res.write('Hello World');
+//         res.end();
+//     }
+//     if(req.url === '/api/courses'){
+//         res.write(JSON.stringify([1,2,3]));
+//         res.end();
+//     }
+// });
+
+// server.listen(3000);
+// console.log('listening on port 3000');
+
+var express = require('express');
+var app = express();   // express object
+
+app.get('/',(req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req,res) => {
+    res.send(([1,2,3]));
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
