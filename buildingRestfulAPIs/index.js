a18e0f0dbb03198802038b45af8538b58c0b6214
tejaktr18 <tejaktr18@gmail.com>
2022-06-25 23:44:05 -0400

    Handling Http Get requests

--- buildingRestfulAPIs/index.js
+++ buildingRestfulAPIs/index.js
@@ -18,15 +18,21 @@ var express = require('express');
 var app = express();   // express object
 
 const port = process.env.PORT || 3000         // PORT is an environment variable
+
+const courses = [{'id': 1, 'name': 'course1'},{'id': 2, 'name': 'course2'}, {'id': 3, 'name': 'course3'}]
 app.get('/',(req, res) => {
     res.send(JSON.stringify(port));
 });
 
 app.get('/api/courses', (req,res) => {
-    res.send(([1,2,3,4,5,6]));
+    res.send(courses);
 });
 
-app.get('/api/courses/:id/:name', (req,res) => {
+app.get('/api/courses/:id', (req,res) => {
+    let cid = courses.find(element => element.id === parseInt(req.params.id));
+    if(cid) res.send(cid);
+    res.status(404).send('Course id not found');
+    
     res.send({"params": req.params,"query": req.query});
     // res.send(req.query);
 });
