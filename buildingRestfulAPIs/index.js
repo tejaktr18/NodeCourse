0c7b7ca858579a81fe4f66e80f0f0d2dc40377cb
tejaktr18 <tejaktr18@gmail.com>
2022-06-26 00:10:35 -0400

    Handling Http Post

--- buildingRestfulAPIs/index.js
+++ buildingRestfulAPIs/index.js
@@ -17,9 +17,11 @@
 var express = require('express');
 var app = express();   // express object
 
+app.use(express.json());
+
 const port = process.env.PORT || 3000         // PORT is an environment variable
 
-const courses = [{'id': 1, 'name': 'course1'},{'id': 2, 'name': 'course2'}, {'id': 3, 'name': 'course3'}]
+const courses = [{id: 1, name: 'course1'},{id: 2, name: 'course2'}, {id: 3, name: 'course3'}]
 app.get('/',(req, res) => {
     res.send(JSON.stringify(port));
 });
@@ -29,12 +31,21 @@ app.get('/api/courses', (req,res) => {
 });
 
 app.get('/api/courses/:id', (req,res) => {
-    let cid = courses.find(element => element.id === parseInt(req.params.id));
-    if(cid) res.send(cid);
+    let course = courses.find(element => element.id === parseInt(req.params.id));
+    if(course) res.send(course);
     res.status(404).send('Course id not found');
     
-    res.send({"params": req.params,"query": req.query});
+    // res.send({"params": req.params,"query": req.query});
     // res.send(req.query);
+});
+
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
